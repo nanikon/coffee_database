41001fba61233fa81147d0b1e243c834b3f0d36c
DELL <milkchocolatewithdaisy@gmail.com>
2020-01-09 20:53:46 +0400

    version for "Cappuccino"

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ import sqlite3
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
 
+NEW_LINE = []
+
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
+        self.d = Dialog_dop()
         self.loadTable()
+        self.pushButton.clicked.connect(self.dialog)
+
+    def dialog(self):
+        self.d.show()
 
     def loadTable(self):
         con = sqlite3.connect("coffee.db")
@@ -17,6 +24,7 @@ class MyWidget(QMainWindow):
         names = list(map(lambda x: x[0], con.execute('SELECT * FROM sorts').description))
         self.tableWidget.setColumnCount(len(names))
         self.tableWidget.setHorizontalHeaderLabels(names)
+        self.tableWidget.setRowCount(0)
         for i, elem in enumerate(result):
             self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
             self.tableWidget.setItem(i, 0, QTableWidgetItem(str(elem[0])))
@@ -30,6 +38,34 @@ class MyWidget(QMainWindow):
             self.tableWidget.setItem(i, 6, QTableWidgetItem(str(elem[6])))
         self.tableWidget.resizeColumnsToContents()
         con.close()
+
+
+class Dialog_dop(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+        name = self.lineEdit.text()
+        stepen = self.comboBox.currentText()
+        pomol = self.comboBox_2.currentText()
+        description = self.plainTextEdit.toPlainText()
+        price = self.spinBox_2.value()
+        volume = self.spinBox.value()
+        if name and stepen and pomol and price and volume:
+            con = sqlite3.connect("coffee.db")
+            cur = con.cursor()
+            stepen = cur.execute("""SELECT id FROM degrees_of_roasting WHERE title = ?""", (stepen,)).fetchone()[0]
+            pomol = cur.execute("""SELECT id FROM pomols WHERE title = ?""", (pomol,)).fetchone()[0]
+            id = max(cur.execute("""SELECT id FROM sorts""").fetchall())[0] + 1
+            cur.execute("""INSERT OR REPLACE INTO sorts VALUES(?, ?, ?, ?, ?, ?, ?)""",
+                        (id, name, stepen, pomol, description, price, volume))
+            con.commit()
+            con.close()
+            ex.loadTable()
+            self.close()
+
 
 app = QApplication(sys.argv)
 ex = MyWidget()
